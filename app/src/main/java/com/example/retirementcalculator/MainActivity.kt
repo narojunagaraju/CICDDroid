b24d9fbdc7a26252da9228e50e9feca2f528d317
Nagaraj <android@mrmsbeauty.com>
2021-11-17 13:05:29 +0530

    analytics added

--- app/src/main/java/com/example/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/example/retirementcalculator/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.retirementcalculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
@@ -8,13 +10,57 @@ import com.microsoft.appcenter.crashes.Crashes
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var calculateButton: Button
+    private lateinit var interestEditText: EditText
+    private lateinit var ageEditText: EditText
+    private lateinit var retirementEditText: EditText
+    private lateinit var monthlySavingsEditText: EditText
+    private lateinit var currentEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculateButton = findViewById(R.id.calculateButton)
+        interestEditText = findViewById(R.id.interestEditText)
+        ageEditText = findViewById(R.id.ageEditText)
+        retirementEditText = findViewById(R.id.retirementEditText)
+        monthlySavingsEditText = findViewById(R.id.monthlySavingsEditText)
+        currentEditText = findViewById(R.id.currentEditText)
 
         AppCenter.start(
             application, "fb555114-a0ee-4e85-89fe-74ccc8583f60",
             Analytics::class.java, Crashes::class.java
         )
+
+        calculateButton.setOnClickListener {
+            //Crashes.generateTestCrash()
+            try {
+                val interestRate = interestEditText.text.toString().toFloat()
+                val currentAge = ageEditText.text.toString().toInt()
+                val retirementAge = retirementEditText.text.toString().toInt()
+                val monthly = monthlySavingsEditText.text.toString().toFloat()
+                val current = currentEditText.text.toString().toFloat()
+
+                val properties: HashMap<String, String> = HashMap()
+                properties["interest_rate"] = interestRate.toString()
+                properties["current_age"] = currentAge.toString()
+                properties["retirement_age"] = retirementAge.toString()
+                properties["monthly_savings"] = monthly.toString()
+                properties["current_savings"] = current.toString()
+
+                if (interestRate <= 0) {
+                    Analytics.trackEvent("wrong_interest_rate")
+                }
+                if (retirementAge <= currentAge) {
+                    Analytics.trackEvent("wrong_age")
+                }
+            } catch (ex: Exception) {
+                Analytics.trackEvent(ex.message)
+            }
+
+
+        }
     }
 }
